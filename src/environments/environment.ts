5bc228e2f0815feb1f9c81d2b48e750ea5119d22
AmrKhaled77 <amrk4912@gmail.com>
2025-09-16 02:55:44 +0300

    enviromnts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     
     baseURL:"https://ecommerce.routemisr.com/api/v1/",
-    host:"http://localhost:4200",
+    host:"https://angular-ecommerce-app-iota.vercel.app",
 };
